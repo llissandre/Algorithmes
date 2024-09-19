815e4ebe4d2d1a885be4be5bf16a7eea7aa16635
llissandreCRM <118750600+llissandre@users.noreply.github.com>
2024-09-19 16:45:30 +0200

    Ajoutez des fichiers projet.

---
+++ Les conditions_2a.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Les_conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 3, B = 4;
+            if (A >= B)
+            {
+                Console.WriteLine($"{A}, {B}");
+            }
+            else
+            {
+                Console.WriteLine($"{B}, {A}");
+            }
+        }
+    }
+}
